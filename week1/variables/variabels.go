e0f9f117b9d9624f672163bb3f08eedcc4f02543
Abhishek Ranjan <125489254+ranjanow@users.noreply.github.com>
2026-03-31 02:38:26 +0530

    Add new Go file variabels.go with example code

---
+++ week1/variables/variabels.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func main() {
+	//step 1
+	//var num = 2
+	//fmt.Print(num)
+
+	//step 2
+	//var num1 = 2
+	//var num2 = 3
+	//var result = num1 + num2
+	//fmt.Print(result)
+
+	//step 3
+	//var num1 int
+	//var num2 int = 3
+	//var result = num1 + num2
+	//fmt.Print(result)
+
+	//step 4
+	//var num1 int
+	//var num2 int = 3
+	//num1 = 2
+	//var result = num1 + num2
+	//fmt.Print(result)
+
+	//step 5
+	//var num1 int
+	//var num2 int
+	//num1 = 2
+	//num2 = 3
+	//var result = num1 + num2
+	//fmt.Print(result)
+
+	//step 6
+	//var num1, num2 int
+	//num1 = 2
+	//num2 = 3
+	//var result = num1 + num2
+	//fmt.Print(result)
+
+	//step 7
+	//var num1, num2 int
+	//num1, num2 = 2, 3
+	//var result = num1 + num2 //result := num1 + num2 (short hand)
+	//fmt.Print(result)
+
+	//step 8
+	//var num = 2
+	//num= 7
+	//fmt.Print(num)
+
+	const num = 2
+
+	fmt.Print(num)
+}
